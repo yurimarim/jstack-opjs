4c47075ec6782dd56950db3d6cd7ebee9ccf7bc1
yurimarim <yurimarim@hotmail.com>
2022-11-18 01:08:10 -0300

    update App.tsx

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,3 +1,13 @@
+import { GlobalStyles } from './styles/GlobalStyles';
+import { Header } from './components/Header';
+import { Orders } from './components/Orders';
+
 export function App() {
-  return <h1>hello world</h1>;
+  return (
+    <>
+      <GlobalStyles />
+      <Header />
+      <Orders />
+    </>
+  );
 }
